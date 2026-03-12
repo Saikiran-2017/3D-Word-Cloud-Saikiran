943be7470fbb1522ae5343b7bae86677fefc604b
Saikiran-2017 <ksai41530@gmail.com>
2026-03-12 13:43:30 -0500

    set up React + TypeScript frontend with Vite and Three.js dependencies

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>3D Word Cloud</h1>
+      <p>Enter a news article URL to visualize its topics.</p>
+    </div>
+  );
+}
+
+export default App;
